e4d2c6dc95fd48aa0f616ce609b8ea145fa3d33d
Dao Pham <daopm.dean@gmail.com>
2024-01-24 23:41:50 +0700

    health status

--- biz/health-check.go
+++ biz/health-check.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"github.com/daopmdean/budgetflows-go-v2/conf"
+	"github.com/daopmdean/budgetflows-go-v2/model"
 	"github.com/daopmdean/summer/common"
 )
 
@@ -11,10 +12,11 @@ func HealthCheck() *common.Response {
 	return &common.Response{
 		Status: common.ResponseStatus.Success,
 		Data: []any{
-			map[string]any{
-				"message":   "I'm alive!",
-				"startTime": conf.AppConfig.ServerStartTime,
-				"timeNow":   time.Now(),
+			model.HealthStatus{
+				Message:     "I'm alive!",
+				StartedTime: conf.AppConfig.ServerStartTime,
+				AppName:     "budgetflows-go-v2",
+				TimeNow:     time.Now(),
 			},
 		},
 	}
---
+++ model/health-status.go
@@ -0,0 +1,10 @@
+package model
+
+import "time"
+
+type HealthStatus struct {
+	Message     string    `json:"message"`
+	StartedTime time.Time `json:"timeStarted"`
+	TimeNow     time.Time `json:"timeNow"`
+	AppName     string    `json:"appName"`
+}
