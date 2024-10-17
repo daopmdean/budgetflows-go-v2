912d0a9a177670a75154faaa9971d003227aface
Pham Minh Dao <daopm.dean@gmail.com>
2024-10-17 17:59:18 +0700

    update user role

--- entity/user_role.go
+++ entity/user_role.go
@@ -11,6 +11,11 @@ type UserRole struct {
 	CreatedTime *time.Time          `json:"createdTime,omitempty" bson:"created_time,omitempty"`
 	UpdatedTime *time.Time          `json:"updatedTime,omitempty" bson:"updated_time,omitempty"`
 
-	UserId  *primitive.ObjectID   `json:"userId,omitempty" bson:"user_id,omitempty"`
-	RoleIds []*primitive.ObjectID `json:"roleIds,omitempty" bson:"role_ids,omitempty"`
+	UserId *primitive.ObjectID `json:"userId,omitempty" bson:"user_id,omitempty"`
+	Roles  []*UserRoleDetail   `json:"roles,omitempty" bson:"roles,omitempty"`
+}
+
+type UserRoleDetail struct {
+	RoleId     *primitive.ObjectID `json:"roleId,omitempty" bson:"role_id,omitempty"`
+	ValidUntil *time.Time          `json:"validUntil,omitempty" bson:"valid_until,omitempty"`
 }
